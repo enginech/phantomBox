72783ad2ba46a76fb81b6fbaf99836d79a28d798
huic <xmuch@qq.com>
2016-06-01 19:32:19 +0800

    'del.phantomjs'

--- phantomjs/src/qt/qtwebkit/Source/qdoc_wrapper.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SetLocal EnableDelayedExpansion
-(set QT_VERSION=5.6.1)
-(set QT_VER=5.6)
-(set QT_VERSION_TAG=561)
-(set QT_INSTALL_DOCS=F:/phantomjs/src/qt/qtbase/doc)
-F:\phantomjs\src\qt\qtbase\bin\qdoc.exe %*
-EndLocal
